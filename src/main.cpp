e7c3a7dec124cd1baf26189a2378bbee7c8ea8f3
Rachit Jain <rachit.jain2095@gmail.com>
2021-08-04 18:51:42 +0000

    added usage of nlohmann via cmakelists.txt and git submodule add <> path

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 #include "utils/math.h"
+#include <nlohmann/json.hpp>
 
+using json = nlohmann::json;
 int main() {
 	std::cout << "hi world ok" << std::endl;
 	std::cout << utils::add(3, 4) << std::endl;
+	// store a string in a JSON value
+	json j2 = {
+		{"pi", 3.141},
+		{"happy", true},
+		{"name", "Niels"},
+		{"nothing", nullptr},
+		{"answer", {
+			{"everything", 42}
+		}},
+		{"list", {1, 0, 2}},
+		{"object", {
+			{"currency", "USD"},
+			{"value", 42.99}
+		}}
+	};
+	// retrieve the serialized value (explicit JSON serialization)
+	std::string serialized_string = j2.dump();
+
+	std::cout << serialized_string << std::endl;
 	return 0;
 }
